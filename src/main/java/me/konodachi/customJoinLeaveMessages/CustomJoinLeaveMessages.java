fdfcdc4daefa5539880cded377f41eece80dec74
konodachi <ekaandeniz16@gmail.com>
2026-02-06 18:37:05 +0300

    Registered the Listeners

--- src/main/java/me/konodachi/customJoinLeaveMessages/CustomJoinLeaveMessages.java
+++ src/main/java/me/konodachi/customJoinLeaveMessages/CustomJoinLeaveMessages.java
@@ -1,17 +1,27 @@
 package me.konodachi.customJoinLeaveMessages;
 
+import me.konodachi.customJoinLeaveMessages.Listeners.JoinListener;
+import me.konodachi.customJoinLeaveMessages.Listeners.LeaveListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CustomJoinLeaveMessages extends JavaPlugin {
+    private static CustomJoinLeaveMessages instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+        getServer().getPluginManager().registerEvents(new LeaveListener(), this);
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static CustomJoinLeaveMessages getInstance() {
+        return instance;
     }
 }
