1d628f4b89f7a1b61e2a7fa7db7e223fcbafd35d
konodachi <ekaandeniz16@gmail.com>
2026-02-07 14:32:58 +0300

    implemented foundation for config reload functionality in the listener classes

--- src/main/java/me/konodachi/customJoinLeaveMessages/Listeners/JoinListener.java
+++ src/main/java/me/konodachi/customJoinLeaveMessages/Listeners/JoinListener.java
@@ -1,7 +1,6 @@
 package me.konodachi.customJoinLeaveMessages.Listeners;
 
 import me.konodachi.customJoinLeaveMessages.CustomJoinLeaveMessages;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -9,19 +8,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.jspecify.annotations.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class JoinListener implements Listener {
     CustomJoinLeaveMessages plugin;
     Set<String> permissions;
+    Map<String, String> roleMessageDict =  new HashMap<>();
 
     public JoinListener(@NonNull CustomJoinLeaveMessages plugin) {
         this.plugin = plugin;
-        permissions = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("roles")).getKeys(false);
+        reloadConfig(plugin);
     }
 
-
+    public void reloadConfig(CustomJoinLeaveMessages plugin){
+        permissions = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("roles")).getKeys(false);
+        roleMessageDict = new HashMap<>();
+        // TODO: change placeholder message to an actual default message
+        for (String role : permissions){
+            roleMessageDict.put(role, plugin.getConfig().getString("roles." + role + ".join-message", "Placeholder Message"));
+        }
+    }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
--- src/main/java/me/konodachi/customJoinLeaveMessages/Listeners/LeaveListener.java
+++ src/main/java/me/konodachi/customJoinLeaveMessages/Listeners/LeaveListener.java
@@ -8,19 +8,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.jspecify.annotations.NonNull;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
 public class LeaveListener implements Listener {
     CustomJoinLeaveMessages plugin;
     Set<String> permissions;
+    Map<String, String> roleMessageDict;
 
     public LeaveListener(@NonNull CustomJoinLeaveMessages plugin) {
         this.plugin = plugin;
-        permissions = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("roles")).getKeys(false);
+        reloadConfig(plugin);
     }
 
-
+    // TODO: Make a proper default message
+    public void reloadConfig(CustomJoinLeaveMessages plugin) {
+        permissions = Objects.requireNonNull(plugin.getConfig().getConfigurationSection("roles")).getKeys(false);
+        roleMessageDict = new HashMap<>();
+        for (String role : permissions) {
+            roleMessageDict.put(role,  plugin.getConfig().getString("roles." + role + ".leave-message", "Placeholder Message"));
+        }
+    }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event){
